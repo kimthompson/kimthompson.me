5c9ba92e7ced8276b3ba68a73771b99d118d08a3
Kim Thompson <kimthompson@hey.com>
2022-09-07 15:04:23 -0500

    :tada: it works!
    and it doesn't even look that embarassing

---
+++ app/routes/blog.tsx
@@ -0,0 +1,22 @@
+import styles from 'highlight.js/styles/tokyo-night-dark.css'
+import { LinksFunction } from '@remix-run/node'
+import { Outlet } from '@remix-run/react'
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'stylesheet', 
+      href: styles,
+    }
+  ]
+}
+
+export default function Blog() {
+  return (
+    <div className="flex justify-center">
+      <article className="prose lg:prose-xl py-10">
+        <Outlet />
+      </article>
+    </div>
+  )
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,5 @@
+export default function Index() {
+  return (
+    <h1 className="text-3xl font-bold underline">Kim Thompson</h1>
+  );
+}
