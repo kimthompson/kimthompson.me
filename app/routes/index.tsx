5363577ea6446c319b01bc8d7c5812b84f284142
Kim Thompson <kimthompson@hey.com>
2022-09-08 09:14:48 -0500

    :bug: sort on date
    sort posts on date

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -17,6 +17,12 @@ function postFromModule(mod: any) {
   }
 }
 
+function sortPostsByDate(a: any, b: any) {
+  let first: any = new Date(a.date)
+  let second: any = new Date(b.date)
+  return second - first
+}
+
 export const loader: LoaderFunction = () => {
   return {
     posts: [postFromModule(firstPost), postFromModule(secondPost)],
@@ -30,10 +36,10 @@ export default function Index() {
   return (
     <main className="mx-6 sm:mx-24 lg:mx-36 xl:mx-48 2xl:mx-96">
       <h2 className="font-heading text-3xl italic text-gray-900 dark:text-slate-100 px-2 pb-6">Recent Posts</h2>
-      <Posts posts={data.posts} />
+      <Posts posts={data.posts.sort(sortPostsByDate)} />
 
       <h2 className="font-heading text-3xl italic text-gray-900 dark:text-slate-100 px-2 pb-6">Drafts <span className="text-base not-italic text-indigo-500">Because privacy is for cowards <a className="hover:text-gray-900 dark:hover:text-slate-100" href="https://www.eff.org">/s</a></span></h2>
-      <Posts posts={data.drafts} isDraft={true} />
+      <Posts posts={data.drafts.sort(sortPostsByDate)} isDraft={true} />
     </main>
   )
 }
