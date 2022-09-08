47e1cb75d03c5cf978bef12ca269d6240df46914
Kim Thompson <kimthompson@hey.com>
2022-09-08 12:59:02 -0500

    :lipstick: fixed mobile styles on blog posts, made YouTube iframe adjust its size
    fixed mobile styles on blog posts, made YouTube iframe adjust its size

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,3 @@
-// import { PaperClipIcon } from '@heroicons/react/24/solid'
 import {
   FiPaperclip,
   FiGithub,
@@ -11,28 +10,33 @@ import {
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center py-12 text-gray-900 dark:text-slate-100">
-      <a href="/assets/resume.pdf">
-        <FiPaperclip className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
-      </a>
-      <a href="https://github.com/kimthompson">
-        <FiGithub className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
-      </a>
-      <a href="mailto:kim.thompson.dev@proton.me">
-        <FiMail className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
-      </a>
-      <a href="https://www.linkedin.com/in/kimberlyannthompson/">
-        <FiLinkedin className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
-      </a>
-      <a href="https://www.youtube.com/channel/UCfXT3usekugleJdBo2LIjig">
-        <FiYoutube className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
-      </a>
-      <a href="https://www.twitch.tv/kimicalburn">
-        <FiTwitch className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
-      </a>
-      <a href="https://twitter.com/Kimical_Burn">
-        <FiTwitter className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
-      </a>
+    <footer className="py-12 text-gray-900 dark:text-slate-100">
+      <div className="flex items-center justify-center mb-6">
+        <a href="/assets/resume.pdf">
+          <FiPaperclip className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
+        </a>
+        <a href="https://github.com/kimthompson">
+          <FiGithub className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
+        </a>
+        <a href="mailto:kim.thompson.dev@proton.me">
+          <FiMail className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
+        </a>
+        <a href="https://www.linkedin.com/in/kimberlyannthompson/">
+          <FiLinkedin className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
+        </a>
+      </div>
+      <div className="flex items-center justify-center">
+        <a href="https://www.youtube.com/channel/UCfXT3usekugleJdBo2LIjig">
+          <FiYoutube className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
+        </a>
+        <a href="https://www.twitch.tv/kimicalburn">
+          <FiTwitch className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
+        </a>
+        <a href="https://twitter.com/Kimical_Burn">
+          <FiTwitter className="h-8 w-8 p-1 mx-3 rounded-md text-slate-100 dark:text-gray-900 transition ease-in-out bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:-translate-y-1 hover:scale-110 duration-300" />
+        </a>
+      </div>
     </footer>
+    
   )
 }
--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -13,10 +13,10 @@ export const links: LinksFunction = () => {
 
 export default function Blog() {
   return (
-    <div className="flex justify-center">
+    <main className="mx-6 sm:mx-24 lg:mx-36 xl:mx-48 2xl:mx-96">
       <article className="prose lg:prose-xl prose-slate dark:prose-invert prose-img:rounded-md  prose-headings:font-heading prose-headings:italic prose-headings:font-light">
         <Outlet />
       </article>
-    </div>
+    </main>
   )
 }
